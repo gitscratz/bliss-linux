d2956453835f2b942beac406705181b037fdd936
gitscratz <jeffreylanzarotti@gmail.com>
2022-02-10 04:41:20 -0600

    adding json files

--- demo.js
+++ demo.js
@@ -1,4 +1,13 @@
 var _ = require('lodash')
+var fs = require('fs')
+var data = required('./data.json')
 
 
-console.log(_.random(1,100))
+console.log("Here's a randeem fukeen numbah! " + _.random(1,100))
+
+fs.readfile('./data.json', 'utf-8', (err,data) => {
+ 	console.log(data)
+})
+
+console.log("this is the end?")
+
