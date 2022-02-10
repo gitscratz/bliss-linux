22a60b859a04c00d71c1e9919496f54bcc1d74c2
gitscratz <jeffreylanzarotti@gmail.com>
2022-02-10 07:26:44 -0600

    more practicing

--- demo.js
+++ demo.js
@@ -1,13 +1,17 @@
 var _ = require('lodash')
 var fs = require('fs')
-var data = required('./data.json')
+var data = require('./data.json')
 
 
 console.log("Here's a randeem fukeen numbah! " + _.random(1,100))
 
-fs.readfile('./data.json', 'utf-8', (err,data) => {
- 	console.log(data)
+fs.readFile('./data.json', 'utf-8', (err,data) => {
+	var data = JSON.parse(data)
+ 	console.log("here is the secret..sshhh... "+data.token)
 })
 
-console.log("this is the end?")
+console.log("this is the end?, "+data.name+" ?")
 
+fs.readdir('/home/scratz', (err,data) => {
+	console.log("directory listing "+data)
+})
